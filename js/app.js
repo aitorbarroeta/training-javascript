60d41c68bf55ed9e06a05adec32f46f2b7962f27
aitor barroeta <aitor2pcpi@gmail.com>
2023-12-06 15:42:31 +0100

    javascript training while and for

--- js/app.js
+++ js/app.js
@@ -104,7 +104,7 @@
 
 // console.log("Today`s date and time")
 // console.log(fullDate)
-
+// // solucion del ejercicio season.js
 // //primera solucion posible
 // if (month== 3 || month== 4 || month== 5){
 //   alert("Spring")
@@ -131,24 +131,184 @@
 //   console.log("Autumn");
 // }
 
-// switch  compara con cada uno de los case que ponemos y el break es para terminar cada case 
+// switch  compara con cada uno de los case que ponemos y el break es para terminar cada case
 // default seria lo equivalente al else es decir si no encuentra true en los demas usara el default
-let character = "Black Widow";
-switch (character) {
-  case "Capitan America":
-    power = "Indestructible shield";
-    break;
-  case "Black Widow":
-    power = "Master spy & assassin";
-    break;
-  case "Iron man":
-    power = "Genius-level intellect";
-    break;
-  case "Thor":
-    power = "The hammer of Thor";
-    break;
-  default:
-    console.log("Choose your hero!");
-    break;
+// let character = "Black Widow";
+// switch (character) {
+//   case "Capitan America":
+//     power = "Indestructible shield";
+//     break;
+//   case "Black Widow":
+//     power = "Master spy & assassin";
+//     break;
+//   case "Iron man":
+//     power = "Genius-level intellect";
+//     break;
+//   case "Thor":
+//     power = "The hammer of Thor";
+//     break;
+//   default:
+//     console.log("Choose your hero!");
+//     break;
+// }
+// console.log(power);
+// // solucion del ejercicio lenguaje.js
+// let lenguage = prompt("seleciona un lenguaje entre en | de | po | pt");
+// switch (lenguage) {
+//   case "en":
+//     answer = "Language in English, Execute code for English";
+//     alert("Language in English, Execute code for English");
+//     break;
+//   case "de":
+//     answer= "Language in Aleman, Execute code for Aleman";
+//     alert("Language in Aleman, Execute code for Aleman");
+//     break;
+//   case "po":
+//     answer= "Language in Polaco, Execute code for Polaco";
+//     alert("Language in Polaco, Execute code for Polaco");
+//     break;
+//   case "pt":
+//     answer = "Language in Portugues, Execute code for Portugues";
+//     alert("Language in Portugues, Execute code for Portugues");
+//     break;
+
+//   default:
+//     alert("Language not chosen, using Spanish");
+//     break;
+// }
+// // solucion del ejercicio Number.js
+// let numero = 35;
+// switch (true) {
+//   case numero < 0:
+//     result = "el numero es menor a 0";
+//     console.log("el numero es menor a 0");
+//     break;
+//   case numero >= 0 && numero <= 10:
+//     result = "el numero esta entre 0 y 10";
+//     console.log("el numero esta entre 0 y 10");
+//     break;
+
+//   case numero >= 11 && numero <= 20:
+//     result = "el numero esta entre 11 y 20";
+//     console.log("el numero esta entre 11 y 20");
+//     break;
+
+//     case  numero > 20:
+//       result = "el numero es mayor que 20";
+//       console.log("el numero es mayor que 20");
+//       break;
+//   default:
+//     console.log("Not is a correct number");
+//     break;
+// }
+// bucle contando de uno en uno
+// let count = 0;
+// while (count < 10) {
+//   console.log(count);
+//   count++;
+
+// }
+// bucle contando de 2 en 2
+// let number = 0;
+// while (number < 10) {
+//   console.log(number);
+//   number += 2;
+// }
+// // bucle contando de 5 en 5
+// number = 0;
+// while (number < 100) {
+//   console.log(number);
+//   number += 5;
+// }
+// // bucle restando de 5 en 5
+
+// while (number >= 0) {
+//   console.log(number);
+//   number -= 5;
+// }
+
+// multiplicando por 2
+// let number = 2;
+// while (number <= 256) {
+//   console.log(number);
+//   number *= 2;
+// }
+// cuenta atras
+// let counter = 10;
+// let timer = setInterval(() => {
+//   console.log(counter);
+//   counteṛ--;
+//   if (counter < 0) {
+//     clearInterval(timer);
+//   }
+// }, 1000)
+// cuenta atras de 10 en un bucle
+// let number = 10;
+// while (number > 0){
+//   number--;
+//   console.log(number);
+// }
+// crear la tabla de multiplicar del 2
+// opcion 1
+// let number = 2;
+// let multi = 1;
+// while (multi <=10)  {
+//   console.log(number,"x",multi,"=",number*multi)
+//   multi++;
+// }
+// opcion 2 visiblemente mas estetica
+// let number = 2;
+// let multi = 1;
+// while (multi <=10)  {
+//   console.log(`${number} x ${multi} = ${number*multi}`)
+//   multi++;
+// }
+//opcion 3 con menos codigo e igual de estetico que la opcion 2
+// let number = 2;
+// let multi = 0;
+// while (multi++ <10)  {
+//   console.log(`${number} x ${multi} = ${number*multi}`)
+// }
+// crear un programa que genere las tablas de multiplicar y su enunciado
+
+// for (let number = 1; number <= 10; number++) {
+//   console.log("Tabla del: ",number);
+//   console.log("----------------");
+//   let multi = 1;
+// while (multi <= 10) {
+//   console.log(number,"x",multi,"=",number*multi);
+//   multi++;
+
+// }
+//   console.log(`----------------`)
+// }
+
+// ejemplo sin corregir en clase
+// let limit = 10;
+// for (let counter = 0; counter  <= limit; counter++) {
+//    console.log(counter);
+// }
+// ejemplo del temario
+// let i,
+// j,
+// sum = 0;
+// for (i = 0, j = 10; i < 10; i++, j--) {
+//   sum += i * j;
+//   console.log(sum);
+// }
+// ejemplo para sacar los numero pares
+// let counter = 1
+// while (counter++ <= 100){
+//   if(counter % 2 == 0){
+
+//     console.log(counter);
+//   }
+// }
+// ejemplo para sacar los numero impares
+let counter = 1
+while (counter++ <= 100){
+  if(counter % 2 != 0){
+
+    console.log(counter);
+  }
 }
-console.log(power);
