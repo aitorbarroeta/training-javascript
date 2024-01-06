7638d757e2b32043bdc90b05e21b6de60fce3a7a
aitor barroeta <aitor2pcpi@gmail.com>
2024-01-06 17:05:13 +0100

    mas ejemplos en el app.js

---
+++ js/algorithms2.js
@@ -0,0 +1,17 @@
+//ejercicio 1
+
+// ejercicio 2
+// let fechaActual = new Date();
+// let name = prompt("Indique su nombre")
+// let time = fechaActual.getHours();
+// console.log(time);
+// if (time < 5 && time >12 ) {
+//     console.log(`Good Morning, ${name}!`);
+    
+// } else if (time >= 12 && time < 18) {
+//     console.log(`Good Afternoon, ${name}!`);
+// } else{
+//     console.log(`Good Evening, ${name}!`);
+// }
+
+//ejercicio 3
